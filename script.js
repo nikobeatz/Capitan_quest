4d0d1c037dd05d6dcf74f6f45bf53dcba9c7c9e4
nikobeatz <nikobeatz91205@gmail.com>
2019-09-19 20:05:26 +0300

    end main page for big devices

--- script.js
+++ script.js
@@ -18,32 +18,53 @@ open.addEventListener("click", function () {
 
 //start card carousel 
 
-$('.birthday-wrapper_wave-items').slick({
-  centerMode: true,
-  centerPadding: '0px',
-  slidesToShow: 3,
-  focusOnSelect: true,
-  infinite: true,
-  responsive: [{
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
+
+$(document).ready(function(){
+  
+  $('.birthday-wrapper_wave-items').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 5,
+    arrows: true,
+    focusOnSelect: false,
+    infinite: true,
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+    ]
+  });
+
+  $('.slick-track .slick-active:first')
+  .css('opacity', '0.5');
+  
+  $('.slick-track .slick-active:last')
+  .css('opacity', '0.5');
 });
+
+$(".slick-prev").click( function() {
+  // whatever you want
+  $('.slick-track .slick-active:first')
+  .css('opacity', '0.5');
+  
+  $('.slick-track .slick-active:last')
+  .css('opacity', '0.5');
+});
+
 
 //end card carousel
 
@@ -53,3 +74,31 @@ $('.birthday-wrapper_wave-items').slick({
 
 
 //end best slider
+
+
+// map open
+
+var map = document.querySelector('.map');
+var openBtn = document.querySelector('.contacts-wrapper-content__btn');
+map.style.display = 'none'
+
+
+openBtn.addEventListener("click", function () {
+
+  if (map.style.display == 'none') map.style.display = 'block';
+  else map.style.display = 'none'
+
+});
+
+// map close
+
+
+//scroll
+
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+  e.preventDefault();
+});
+
+//end scroll
