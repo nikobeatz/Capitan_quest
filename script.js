5fbe0469603720780a32fb52607df4c7edac69d1
nikobeatz <nikobeatz91205@gmail.com>
2019-09-24 15:47:34 +0300

    add some changes

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ openBtnMenu.addEventListener("click", function () {
 
 //scroll
 
-$("body").on('click', '[href*="#"]', function(e){
+$("html, body").on('click', '[href*="#"]', function(e){
   var fixed_offset = 100;
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
   e.preventDefault();
